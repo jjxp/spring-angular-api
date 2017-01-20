0fc9eadf02e0b970a312e2bb9c03914906dc41a8
jjxp <javgarcal@alum.us.es>
2017-01-20 14:24:07 +0100

    Main Eureka service registration

--- src/main/java/com/sprouts/Application.java
+++ src/main/java/com/sprouts/Application.java
@@ -4,6 +4,7 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 import com.sprouts.util.PopulateDatabase;
 
@@ -11,9 +12,11 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
+@EnableEurekaServer
 public class Application implements ApplicationRunner {
 
 	public static void main(String[] args) {
+		System.setProperty("spring.config.name", "application-server");
 		SpringApplication.run(Application.class, args);
 	}
 
