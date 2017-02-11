5f16c82378757c2a62dbdaf705b0856030538339
jjxp <javgarcal@alum.us.es>
2017-02-11 16:24:03 +0100

    De-hardcoded MySQL datasource in backend (#2)
    The datasource configuration has been included in backend-server.yml instead of being hardcoded when launching the application.

--- backend-server/src/main/java/org/sprouts/backend/BackEndServer.java
+++ backend-server/src/main/java/org/sprouts/backend/BackEndServer.java
@@ -13,15 +13,6 @@ public class BackEndServer implements ApplicationRunner {
 
 	public static void main(String[] args) {
 		System.setProperty("spring.config.name", "backend-server");
-		System.setProperty("spring.datasource.platform","mysql");
-		System.setProperty("spring.datasource.url","jdbc:mysql://localhost:3306/sprouts-core");
-		System.setProperty("spring.datasource.username","manager");
-		System.setProperty("spring.datasource.password","root");
-		System.setProperty("spring.jpa.hibernate.ddl-auto","update");
-		System.setProperty("spring.jpa.hibernate.naming-strategy","org.hibernate.cfg.DefaultNamingStrategy");
-		System.setProperty("spring.datasource.username","manager");
-		System.setProperty("spring.jpa.properties.hibernate.dialect","org.hibernate.dialect.MySQL5Dialect");
-
 		SpringApplication.run(BackEndServer.class, args);
 	}
 
