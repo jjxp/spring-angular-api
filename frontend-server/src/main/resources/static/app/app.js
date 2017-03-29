64ea0a52e0f80831ab05d4a205dea4baae1c5c6b
Daniel Moreno Márquez <danimoreno.m08@gmail.com>
2017-03-29 18:39:05 +0200

    Authentication from frontend
    Created LoginController, which performs the request to the oauth server and receives the token
    Created an interceptor, which puts the token in the header for any request
    Modified Zuul Settings to Allow Authentication Headers

--- frontend-server/src/main/resources/static/app/app.js
+++ frontend-server/src/main/resources/static/app/app.js
@@ -1,32 +1,61 @@
 'use strict';
 
-var angularApp = angular.module('angularApp', [ 'ngRoute', 'angularAppControllers', 'ngSanitize', 'swaggerUi', 'pascalprecht.translate' ]);
+var angularApp = angular.module('angularApp', ['ngRoute', 'angularAppControllers', 'ngSanitize', 'swaggerUi', 'pascalprecht.translate', 'ngResource', 'ngCookies']);
 
-angularApp.controllers = angular.module('angularAppControllers', [ 'ngRoute' ]);
+angularApp.controllers = angular.module('angularAppControllers', ['ngRoute']);
 
-angularApp.config([ '$translateProvider', function($translateProvider) {
-	$translateProvider.useStaticFilesLoader({
-	    files: [{
-	        prefix: 'locale-',
-	        suffix: '.json'
-	    }, {
-	        prefix: '/app/view/user/locale-',
-	        suffix: '.json'
-	    }, {
-	        prefix: '/app/view/car/locale-',
-	        suffix: '.json'
-	    }, {
-	        prefix: '/app/view/api/locale-',
-	        suffix: '.json'
-	    }]
-	});
-	$translateProvider.preferredLanguage('en');
-} ]);
+angularApp.config(['$translateProvider', function ($translateProvider) {
+    $translateProvider.useStaticFilesLoader({
+        files: [{
+            prefix: 'locale-',
+            suffix: '.json'
+        }, {
+            prefix: '/app/view/user/locale-',
+            suffix: '.json'
+        }, {
+            prefix: '/app/view/car/locale-',
+            suffix: '.json'
+        }, {
+            prefix: '/app/view/api/locale-',
+            suffix: '.json'
+        }, {
+            prefix: '/app/view/login/locale-',
+            suffix: '.json'
+        }
+        ]
+    });
+    $translateProvider.preferredLanguage('en');
+}]);
 
-angularApp.controller('languageController', [ '$translate', '$scope',
-		function($translate, $scope) {
+angularApp.config(['$httpProvider', function ($httpProvider) {
+    $httpProvider.interceptors.push(['$q', function ($q) {
 
-			$scope.changeLanguage = function(langKey) {
-				$translate.use(langKey);
-			};
-		} ]);
+        return {
+            request: function (httpConfig) {
+                var $cookies;
+                angular.injector(['ngCookies']).invoke(['$cookies', function (_$cookies_) {
+                    $cookies = _$cookies_;
+                }]);
+
+                if ($cookies.get("access_token")) {
+
+                    httpConfig.headers['Authorization'] = 'Bearer ' + $cookies.get("access_token");
+                    
+                }
+
+                return httpConfig;
+            },
+            responseError: function (response) {
+                return $q.reject(response);
+            }
+        };
+    }]);
+}]);
+
+angularApp.controller('languageController', ['$translate', '$scope',
+    function ($translate, $scope) {
+
+        $scope.changeLanguage = function (langKey) {
+            $translate.use(langKey);
+        };
+    }]);
---
+++ frontend-server/src/main/resources/static/app/controller/loginController.js
@@ -0,0 +1,35 @@
+'use strict';
+(function () {
+    var LoginController = function ($scope, $http, $resource, $httpParamSerializer, $cookies) {
+
+        $scope.login = function () {
+
+            $scope.formRequest = {
+                grant_type: "password",
+                username: $scope.loginCredentials.username,
+                password: $scope.loginCredentials.password
+            };
+            $scope.encoded = btoa("sproutsApp:sproutsAppSecret");
+
+            $cookies.remove("access_token");
+            var req = {
+                method: 'POST',
+                url: "/oauth/token",
+                headers: {
+                    "Authorization": "Basic " + $scope.encoded,
+                    "Content-type": "application/x-www-form-urlencoded; charset=utf-8"
+                },
+                data: $httpParamSerializer($scope.formRequest)
+            }
+            $http(req).then(function (data) {
+                $http.defaults.headers.common.Authorization =
+                    'Bearer ' + data.data.access_token;
+                $cookies.put("access_token", data.data.access_token);
+                window.location.href = "/";
+                console.log(data.data.access_token)
+            });
+        };
+    };
+
+    angularApp.controllers.controller('LoginController', ['$scope', '$http', '$resource', '$httpParamSerializer', '$cookies', LoginController]);
+})();
